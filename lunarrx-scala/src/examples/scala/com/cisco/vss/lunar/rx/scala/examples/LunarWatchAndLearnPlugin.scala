d89880db414d3ab0cea38ed12ba4abac9f3e07c6
Asher Sterkin <asterkin@cisco.com>
2014-03-05 16:41:43 +0200

    Basic word count works

--- lunarrx-scala/src/examples/scala/com/cisco/vss/lunar/rx/scala/examples/LunarWatchAndLearnPlugin.scala
+++ lunarrx-scala/src/examples/scala/com/cisco/vss/lunar/rx/scala/examples/LunarWatchAndLearnPlugin.scala
@@ -2,35 +2,44 @@ package com.cisco.vss.lunar.rx.scala.examples
 
 import com.cisco.vss.lunar.rx.plugin.scala.Lunar
 import com.cisco.vss.lunar.rx.plugin.schema.SubtitlesTrackItem
+import rx.lang.scala.Observable
+import java.net.URL
 
 object LunarWatchAndLearnPlugin {
-  def main(args: Array[String]): Unit = {
-    val HOST         = args(0)
-    val PORT         = Integer.parseInt(args(1))
-    val DEVELOPER_ID = "6871c4b35301671668ebf26ae46b6441"
-    val lunar        = Lunar(HOST, PORT, DEVELOPER_ID)
-    val SOURCE_ID    = "1"
-    val INPUT_PLUGIN = "subtitletext"
-    val INPUT_TRACK  = "subtitles"
-    val ts           = lunar.getInputTrackItemStream(classOf[SubtitlesTrackItem], SOURCE_ID, INPUT_PLUGIN, INPUT_TRACK)
-    
-    ts
-//    .map(p => TsPacket(p))
-//    .filter(tsp => !tsp.hasError)
-//    .filter(tsp => tsp.hasPayloadStart)
-//    .filter(tsp => 0 == tsp.pid)
-//    .map(tsp => PsiPat(tsp))
-//    .distinctUntilChanged
-//    //TODO: write Pat back to Lunar?
-    .subscribe(
-        sub => {
-          println(sub.getTime(), sub.getPts())
-          println(sub.getText())
-        }
-        ,err => {
-          println("Error!",err)
-        }
-       ,() => println("Unexpected EOF")        
-    )    
-  }
+    //TODO: move to Lunar?
+	implicit class Regex(sc: StringContext) {
+	  def r = new util.matching.Regex(sc.parts.mkString, sc.parts.tail.map(_ => "x"): _*)
+	}
+	
+	def main(args: Array[String]): Unit = {
+	    val HOST         = args(0)
+	    val PORT         = Integer.parseInt(args(1))
+	    val DEVELOPER_ID = "6871c4b35301671668ebf26ae46b6441"
+	    val lunar        = Lunar(HOST, PORT, DEVELOPER_ID)
+	    val SOURCE_ID    = "1"
+	    val INPUT_PLUGIN = "subtitletext"
+	    val INPUT_TRACK  = "subtitles"
+	    val ts           = lunar.getInputTrackItemStream(classOf[SubtitlesTrackItem], SOURCE_ID, INPUT_PLUGIN, INPUT_TRACK)
+	    
+	    ts
+	    .map(sub => sub.getText())
+	    .map(text => text.split("[ .,?!']"))
+	    .flatMap(wordList => Observable.from(wordList))
+	    .map(word => word.trim())
+	    .filter(word => word match { case r"\w\w\w+" => true case _ => false})
+	    .map(word => new URL("http","www.wordcount.org",String.format("/dbquery.php?toFind=%s&method=SEARCH_BY_NAME", word)))
+	    .flatMap(url => lunar.synchHttpGet(url))
+	    .map(result => result.split("&"))
+	    .filter(result => result(1) == "wordFound=yes")
+	    .map(result => (result(4).split("=")(1), Integer.parseInt(result(3).split("=")(1))))
+	    .subscribe(
+	        result  => {
+	          println(result)
+	        }
+	        ,err => {
+	          println("Error!",err)
+	        }
+	       ,() => println("Unexpected EOF")        
+	    )    
+	}
 }
--- lunarrx-scala/src/main/scala/com/cisco/vss/lunar/rx/plugin/scala/Lunar.scala
+++ lunarrx-scala/src/main/scala/com/cisco/vss/lunar/rx/plugin/scala/Lunar.scala
@@ -2,6 +2,8 @@ package com.cisco.vss.lunar.rx.plugin.scala
 import com.cisco.vss.lunar.rx.plugin._
 import rx.lang.scala.JavaConversions._
 import rx.lang.scala._
+import java.net.URL
+import com.cisco.vss.rx.java._
 
 abstract class Lunar {
 	private [scala] val asJavaLunar: core.Lunar
@@ -11,6 +13,10 @@ abstract class Lunar {
 
 	def getInputTrackItemStream[T](clazz: Class[T], sourceID: String, pluginName: String, trackName: String): Observable[T] = {
 		toScalaObservable(asJavaLunar.getInputTrackItemStream(clazz, sourceID,pluginName, trackName))
+	}
+	
+	def synchHttpGet(url: URL): Observable[String] = {
+		toScalaObservable(Conversions.synchHttpGet.call(url))
 	}
 }
 
