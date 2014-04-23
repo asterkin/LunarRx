4f0cc98c5f8323e460813ad0437d8d12b124354f
Asher Sterkin <asterkin@cisco.com>
2014-04-23 18:27:50 +0300

    Removed duplications

--- lunarrx-core/src/test/java/com/cisco/vss/lunar/rx/plugin/core/LunarTrackStreamProcessorTest.java
+++ lunarrx-core/src/test/java/com/cisco/vss/lunar/rx/plugin/core/LunarTrackStreamProcessorTest.java
@@ -15,78 +15,72 @@ import rx.Observable;
 
 @RunWith(MockitoJUnitRunner.class)
 public class LunarTrackStreamProcessorTest {
+	private static final LunarTrack RESULT_TRACK = new LunarTrack(1, "pluginA", "trackB");
+	private static final byte[]                    BUFFER    = "abcedefg".getBytes();
+	private static final byte[][]                  RESULTS   = new byte[][] {BUFFER};
+	private static final Observable<byte[]>        RESULT    = Observable.from(RESULTS);
+	
 	@Mock
 	private Lunar          lunar;
 	@Mock
 	private LunarMQWriter  writer;
-	private LunarTrack     resultTrack;
 	
 	@Before
 	public void setUp() {
-		resultTrack = new LunarTrack(1, "pluginA", "trackB");
 	}
 	
 	@Test
 	public void testCall_OK() throws Throwable {
-		final byte[]                    BUFFER    = "abcedefg".getBytes();
-		final byte[][]                  RESULTS   = new byte[][] {BUFFER};
-		final Observable<byte[]>        result    = Observable.from(RESULTS);
-		final LunarTrackStreamProcessor processor = new LunarTrackStreamProcessor(lunar, resultTrack, result);
+		final LunarTrackStreamProcessor processor = new LunarTrackStreamProcessor(lunar, RESULT_TRACK, RESULT);
 		
 		when(writer.call(BUFFER)).thenReturn(Observable.from(BUFFER));
 		processor.call(writer);
 		
 		verify(writer).call(BUFFER);
 		verify(writer).close();
-		verify(lunar).running(resultTrack);
-		verify(lunar).stopped(resultTrack);
-		verify(lunar, never()).stopped(eq(resultTrack), (Throwable)anyObject());
+		verify(lunar).running(RESULT_TRACK);
+		verify(lunar).stopped(RESULT_TRACK);
+		verify(lunar, never()).stopped(eq(RESULT_TRACK), (Throwable)anyObject());
 	}
 
 	@Test
 	public void testCall_ErrorOnWrite() throws Throwable {
-		final byte[]                    BUFFER    = "abcedefg".getBytes();
-		final byte[][]                  RESULTS   = new byte[][] {BUFFER};
-		final Observable<byte[]>        result    = Observable.from(RESULTS);
 		final Exception                 ERROR     = new Exception("Error on write");
 		final Observable<byte[]>        ERROR_OBS = Observable.error(ERROR); 
-		final LunarTrackStreamProcessor processor = new LunarTrackStreamProcessor(lunar, resultTrack, result);
+		final LunarTrackStreamProcessor processor = new LunarTrackStreamProcessor(lunar, RESULT_TRACK, RESULT);
 		
 		when(writer.call(BUFFER)).thenReturn(ERROR_OBS);
 		processor.call(writer);
 		
-		verify(lunar).running(resultTrack);
-		verify(lunar).stopped(resultTrack, ERROR);
-		verify(lunar, never()).stopped(resultTrack);
+		verify(lunar).running(RESULT_TRACK);
+		verify(lunar).stopped(RESULT_TRACK, ERROR);
+		verify(lunar, never()).stopped(RESULT_TRACK);
 	}
 
 	@Test
 	public void testCall_ErrorFromStream() {
 		final Exception                 ERROR     = new Exception("Error from stream");
 		final Observable<byte[]>        result    = Observable.error(ERROR);
-		final LunarTrackStreamProcessor processor = new LunarTrackStreamProcessor(lunar, resultTrack, result);
+		final LunarTrackStreamProcessor processor = new LunarTrackStreamProcessor(lunar, RESULT_TRACK, result);
 		
 		processor.call(writer);
 		
-		verify(lunar).running(resultTrack);
-		verify(lunar).stopped(resultTrack, ERROR);
+		verify(lunar).running(RESULT_TRACK);
+		verify(lunar).stopped(RESULT_TRACK, ERROR);
 		verify(writer, never()).call((byte[])anyObject());
 	}
 	
 	@Test
 	public void testCall_ErrorOnClose() throws IOException {
-		final byte[]                    BUFFER    = "abcedefg".getBytes();
-		final byte[][]                  RESULTS   = new byte[][] {BUFFER};
-		final Observable<byte[]>        result    = Observable.from(RESULTS);
-		final LunarTrackStreamProcessor processor = new LunarTrackStreamProcessor(lunar, resultTrack, result);
+		final LunarTrackStreamProcessor processor = new LunarTrackStreamProcessor(lunar, RESULT_TRACK, RESULT);
 		
 		when(writer.call(BUFFER)).thenReturn(Observable.from(BUFFER));
 		doThrow(new IOException("Error on close")).when(writer).close();
 		processor.call(writer);
 		
 		verify(writer).call(BUFFER);
-		verify(lunar).running(resultTrack);
-		verify(lunar).stopped(resultTrack);
-		verify(lunar, never()).stopped(eq(resultTrack), (Throwable)anyObject());
+		verify(lunar).running(RESULT_TRACK);
+		verify(lunar).stopped(RESULT_TRACK);
+		verify(lunar, never()).stopped(eq(RESULT_TRACK), (Throwable)anyObject());
 	}	
 }
