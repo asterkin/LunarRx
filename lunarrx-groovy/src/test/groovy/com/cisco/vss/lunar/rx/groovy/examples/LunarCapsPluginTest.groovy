b2113b8131b2bc2b72030179ee505615681d1394
Asher Sterkin <asterkin@cisco.com>
2014-04-30 17:56:14 +0300

    fixing gradle issues

--- lunarrx-groovy/src/test/groovy/com/cisco/vss/lunar/rx/groovy/examples/LunarCapsPluginTest.groovy
+++ lunarrx-groovy/src/test/groovy/com/cisco/vss/lunar/rx/groovy/examples/LunarCapsPluginTest.groovy
@@ -3,7 +3,6 @@ package com.cisco.vss.lunar.rx.groovy.examples;
 import static org.junit.Assert.*;
 import org.junit.Test;
 import com.cisco.vss.lunar.rx.plugin.schema.subtitletext.Subtitles;
-import com.cisco.vss.lunar.rx.plugin.schema.subtitletext.Subtitles.*;
 import static com.cisco.vss.lunar.rx.groovy.examples.LunarCapsPlugin.*;
 import rx.Observable;
 
@@ -11,10 +10,10 @@ class LunarCapsPluginTest {
 	@Test
 	public void test() {
 	  def subtitles = new Subtitles()
-	  def line1     = new SubtitleLine(subtitles, "Xxxx yyyy")
-	  def line2     = new SubtitleLine(subtitles, "xxxx Yyyy")
-	  def line3     = new SubtitleLine(subtitles, "\n")
-	  def data      = new Data(subtitles)
+	  def line1     = new Subtitles.SubtitleLine(subtitles, "Xxxx yyyy")
+	  def line2     = new Subtitles.SubtitleLine(subtitles, "xxxx Yyyy")
+	  def line3     = new Subtitles.SubtitleLine(subtitles, "\n")
+	  def data      = new Subtitles.Data(subtitles)
 	   
 	  data.lines    = [line1, line2, line3]
 	  subtitles.data= data
