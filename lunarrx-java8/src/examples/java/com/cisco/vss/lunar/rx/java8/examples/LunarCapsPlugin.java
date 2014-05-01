a41bb35d4e769ef38f3bbb63e10d0199cc05d99b
Asher Sterkin <asterkin@cisco.com>
2014-05-01 11:14:11 +0300

    It's not all good, but works somehow

--- lunarrx-java8/src/examples/java/com/cisco/vss/lunar/rx/java8/examples/LunarCapsPlugin.java
+++ lunarrx-java8/src/examples/java/com/cisco/vss/lunar/rx/java8/examples/LunarCapsPlugin.java
@@ -1,12 +1,41 @@
 package com.cisco.vss.lunar.rx.java8.examples;
 
 import com.cisco.vss.lunar.rx.plugin.core.Lunar;
-import com.cisco.vss.lunar.rx.plugin.schema.subtitletext.*;
 import com.cisco.vss.lunar.rx.plugin.schema.capsrx.*;
+import com.cisco.vss.lunar.rx.plugin.schema.subtitletext.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 import rx.Observable;
 
 class LunarCapsPlugin {
+        final static String[] extractCaps(final List<String> words) {
+            final List<String> caps = words.stream()
+                    .map(word -> word.trim())
+                    .filter(word -> !word.isEmpty())
+                    .filter(word -> Character.isUpperCase(word.charAt(0)))
+                    .collect(Collectors.toList());
+            return caps.toArray(new String[]{});
+        }
+        
 	final static Observable<Caps> buildCaps(final Observable<Subtitles> input) {
-		return null;
+		return input
+                        .map(sub  -> sub.getText())
+                        .map(text -> Arrays.asList(text.split("\\s+")))
+                        .map(words-> extractCaps(words))
+                        .map(caps -> new Caps(caps));
 	}
+        
+        final static String[] getArgs(final String argv[]) {
+            return argv.length>0 ? argv : new String[] {
+                    "6871c4b35301671668ebf26ae46b6441", 
+                    "caprsrx", 
+                    "54.72.89.87", 
+                    "7000"
+            };
+
+        }
+        public final static void main(final String[] args) {
+            new Lunar(getArgs(args)).transform(Subtitles.class, LunarCapsPlugin::buildCaps, Caps.class);
+        }
 }
--- lunarrx-java8/src/test/java/com/cisco/vss/lunar/rx/java8/examples/LunarCapsPluginTest.java
+++ lunarrx-java8/src/test/java/com/cisco/vss/lunar/rx/java8/examples/LunarCapsPluginTest.java
@@ -1,22 +1,27 @@
 package com.cisco.vss.lunar.rx.java8.examples;
 
-import static org.junit.Assert.*;
-
-import java.util.Iterator;
-
-import org.junit.Test;
-
-import com.cisco.vss.lunar.rx.plugin.schema.subtitletext.Subtitles;
-import com.cisco.vss.lunar.rx.plugin.schema.subtitletext.Subtitles.*;
-import com.cisco.vss.lunar.rx.plugin.schema.capsrx.Caps;
 
 import static com.cisco.vss.lunar.rx.java8.examples.LunarCapsPlugin.*;
+import com.cisco.vss.lunar.rx.plugin.schema.capsrx.Caps;
+import com.cisco.vss.lunar.rx.plugin.schema.subtitletext.Subtitles;
+import com.cisco.vss.lunar.rx.plugin.schema.subtitletext.Subtitles.Data;
+import com.cisco.vss.lunar.rx.plugin.schema.subtitletext.Subtitles.SubtitleLine;
+import java.util.Arrays;
+import java.util.Iterator;
+import static org.junit.Assert.*;
+import org.junit.Test;
 import rx.Observable;
 
 public class LunarCapsPluginTest {
 
+        @Test
+        public void testExtractCaps() {
+            final String[] caps = extractCaps(Arrays.asList("Xxxx", "yyyyxxxx", "Yyyy"));
+            assertArrayEquals(new String[]{"Xxxx", "Yyyy"}, caps);
+        }
+        
 	@Test
-	public void test() {
+	public void testBuildCaps() {
 		final Subtitles    subtitles = new Subtitles();
 		final SubtitleLine line1     = subtitles.new SubtitleLine("Xxxx yyyy");
 		final SubtitleLine line2     = subtitles.new SubtitleLine("xxxx Yyyy");
