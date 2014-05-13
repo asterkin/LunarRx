39ce389e98790216007119eb2bb0447eca21465b
Asher Sterkin <asterkin@cisco.com>
2014-05-13 12:02:50 +0300

    reduceUntul changed

--- lunarrx-scala/src/main/scala/com/cisco/vss/lunar/rx/plugin/scala/ObservableEx.scala
+++ lunarrx-scala/src/main/scala/com/cisco/vss/lunar/rx/plugin/scala/ObservableEx.scala
@@ -2,14 +2,14 @@ package com.cisco.vss.lunar.rx.plugin.scala
 import rx.lang.scala._
 
 object ObservableEx {
-	implicit class ObservableOps[T](val obs: Observable[T]) extends AnyVal {
-	  def reduceUntil(init: T, func: (T,T)=>T, pred: (T)=>Boolean): Observable[T] = {
-	    Observable[T](observer => {
-	    	var current: T = init
+	implicit class ObservableOps[T1](val obs: Observable[T1]) extends AnyVal {
+	  def reduceUntil[T2](init: T2, func: (T2,T1)=>T2, pred: (T1)=>Boolean): Observable[T2] = {
+	    Observable[T2](observer => {
+	    	var current: T2 = init
 	    	obs.subscribe(
-	    	    (v: T) => {
+	    	    (v: T1) => {
 	    	      current = func(current, v)
-	    	      if(pred(current)) {
+	    	      if(pred(v)) {
 	    	        observer.onNext(current)
 	    	        current = init
 	    	      }
--- lunarrx-scala/src/test/scala/com/cisco/vss/lunar/rx/plugin/scala/ObservableExTest.scala
+++ lunarrx-scala/src/test/scala/com/cisco/vss/lunar/rx/plugin/scala/ObservableExTest.scala
@@ -10,7 +10,7 @@ class ObservableExTest extends JUnitSuite {
 	@Test def testReduceUntil() {
 	  val input = Observable.from(Array("aa","bbb","ccc\n","dd\n"))
 	  
-	  val result = input.reduceUntil("", (s1,s2) => s1+s2, (s) => '\n'==s.last).toBlockingObservable.toIterable
+	  val result = input.reduceUntil("", (s1:String, s2) => s1+s2, (s) => '\n'==s.last).toBlockingObservable.toIterable
 	  
 	  assert(Array("aabbbccc\n","dd\n").sameElements(result))
 	}
